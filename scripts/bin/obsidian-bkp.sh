872703c3072e259c822e90569ff7d302f1dfc28a
chris <chris-nies@hotmail.de>
2026-03-04 18:30:16 +0100

    scripts: updated obsidian backup script.
    Modified path to downloads instead of nextcloud

--- scripts/bin/obsidian-bkp.sh
+++ scripts/bin/obsidian-bkp.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Specify the directory you want to zip
-DIRECTORY_TO_ZIP="/home/chris/Documents/livesync"
-DESTINATION_DIRECTORY="/home/chris/Nextcloud/04_ARCHIVE/obsidian_bkp"
+DIRECTORY_TO_ZIP="/home/chris/Documents/obsidian/vault/"
+DESTINATION_DIRECTORY="/home/chris/Downloads/"
 # Get the current timestamp in the format YYYYMMDD_HHMMSS
 TIMESTAMP=$(date +"%Y-%m-%d_%H%M%S")
 
 # Create the zip filename with the timestamp
-ZIP_FILENAME="livesync_$TIMESTAMP.zip"
+ZIP_FILENAME="vault_$TIMESTAMP.zip"
 
 # Move to the parent directory of the specified directory
 cd "$(dirname "$DIRECTORY_TO_ZIP")" || exit
 
 # Create the zipped archive
-zip -r "$ZIP_FILENAME" "$(basename "$DIRECTORY_TO_ZIP")"
+zip -r -q "$ZIP_FILENAME" "$(basename "$DIRECTORY_TO_ZIP")"
 
 mv "$ZIP_FILENAME" "$DESTINATION_DIRECTORY"
 
